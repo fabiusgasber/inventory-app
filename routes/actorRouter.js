87fcc0ff6e8c1e7d3771c18bff1006bd8b5554bc
Fabius Gasber <fabiusgasber@gmail.com>
2025-07-28 14:54:07 +0200

    feat(actorRouter): add routes to update actors

--- routes/actorRouter.js
+++ routes/actorRouter.js
@@ -5,5 +5,7 @@ const controller = require("../controllers/actorController")
 actorRouter.get("/", controller.getActors);
 actorRouter.get("/new", controller.actorsCreateGet);
 actorRouter.post("/new", controller.actorsCreatePost);
+actorRouter.get("/:id/edit", controller.actorsUpdateGet);
+actorRouter.post("/:id/edit", controller.actorsUpdatePost);
 
 module.exports = actorRouter;
