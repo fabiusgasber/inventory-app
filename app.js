9546a28103b02c5c4b4becb1cfe5f9f8dff28ad8
Fabius Gasber <fabiusgasber@gmail.com>
2025-08-01 14:09:40 +0200

    style: add layout styling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const indexRouter = require("./routes/indexRouter");
 const movieRouter = require("./routes/movieRouter");
 const genreRouter = require("./routes/genreRouter");
 const actorRouter = require("./routes/actorRouter");
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -14,5 +15,6 @@ app.use("/", indexRouter);
 app.use("/movie", movieRouter);
 app.use("/genre", genreRouter);
 app.use("/actor", actorRouter);
+app.use(express.static(assetsPath));
 
 app.listen(PORT, "0.0.0.0", () => console.log(`Listening on port ${PORT}`));
